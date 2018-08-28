7bd147907c4d10848594c3fb7892ffe5abb0984b
JCSnedeker <jsnedek1@jhu.edu>
2018-08-28 13:42:30 -0400

    Task 3

---
+++ day2-morning/07_Onehit.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv)>1:
+    f = open( sys.argv[1] )
+else:    
+    f=sys.stdin
+
+count = 0
+
+for i, line in enumerate( f ):
+    if i == 0:
+        continue
+    fields = line.rstrip("\r\n").split("\t")
+    if fields[1].startswith("@"):
+        continue
+    if len(fields)>12:
+        continue
+        if fields[20] != "NH:i:1":
+            continue
+    else:
+        count += 1
+
+print(count)
