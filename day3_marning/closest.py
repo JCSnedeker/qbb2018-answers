efd5c4ec781172f3c756c45ea41799c5327d9fff
JCSnedeker <jsnedek1@jhu.edu>
2018-08-29 14:07:01 -0400

    Task 3

---
+++ day3_marning/closest.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+#print uniq names from t_data file
+
+import sys
+
+distance = {}
+my_dist = 0
+
+for line in open(sys.argv[1]):
+    if "#" in line:
+        continue
+    fields=line.rstrip("\r\n").split("\t")
+    types=fields[2]
+    chromosome = fields[0]
+    stringy = fields[8].split()
+    if types != "gene":
+        continue
+    if chromosome != "3R":
+        continue
+    gene_start=int(fields[3])
+    gene_end=int(fields[4])
+    find_pos=21378950
+    if find_pos < gene_start:
+        my_dist = gene_start - find_pos
+    elif find_pos > gene_end:
+        my_dist = find_pos - gene_end
+    if "gene_id" in stringy:
+        biotype_index = stringy.index("gene_id")
+        Next_index = biotype_index +1
+        distance[stringy[Next_index]]=my_dist
+
+
+var=(min(distance, key=distance.get))
+print(var, distance[var])
