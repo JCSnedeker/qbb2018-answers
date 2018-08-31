91c927de981ae24e2a9a414d462b5097de167907
JCSnedeker <jsnedek1@jhu.edu>
2018-08-31 16:35:36 -0400

    Updated Task 3,4, and 5

--- day5-morning/06_whatshappening.py
+++ day5-morning/06_whatshappening.py
@@ -15,19 +15,19 @@ Express = df.loc[:,'FPKM']
 df1=pd.read_csv(sys.argv[1], sep="\t")
 df1_1=df1.iloc[:,-1]
 df2=pd.read_csv(sys.argv[2], sep="\t")
-df2_1=df1.iloc[:,-1]
+df2_1=df2.iloc[:,-1]
 df3=pd.read_csv(sys.argv[3], sep="\t")
-df3_1=df1.iloc[:,-1]
+df3_1=df3.iloc[:,-1]
 df4=pd.read_csv(sys.argv[4], sep="\t")
-df4_1=df1.iloc[:,-1]
+df4_1=df4.iloc[:,-1]
 df5=pd.read_csv(sys.argv[5], sep="\t")
-df5_1=df1.iloc[:,-1]
+df5_1=df5.iloc[:,-1]
 
 His_mod = pd.concat([df1_1, df2_1, df3_1, df4_1, df5_1, Express], axis=1)
-His_mod.columns=["H3K4me3", "H3K4me1", "H3K27me3", "H3K27ac", "H3K9ac", "FPKM"]
+His_mod.columns=["H3K4me1", "H3K4me3", "H3K27me3", "H3K27ac", "H3K9ac", "FPKM"]
+print(His_mod)
 
-
-Regress=sm.ols(formula='FPKM ~ H3K4me3 + H3K4me1 + H3K27me3 + H3K27ac + H3K9ac', data=His_mod)
+Regress=sm.ols(formula='FPKM ~ H3K4me1 + H3K4me3 + H3K27me3 + H3K27ac + H3K9ac', data=His_mod)
 results=Regress.fit()
 print(results.summary())
 
